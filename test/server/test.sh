a4ebfef5d9522383b51303076f86961d927ede20
Harry Brundage <harry.brundage@gmail.com>
2021-06-09 09:43:24 -0400

    Add a couple simple test scripts

--- test/server/test.sh
+++ test/server/test.sh
@@ -2,4 +2,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 set -ex
 
-$DIR/../pkg/esbuild-dev.bin.js $DIR/simple/run.ts
+$DIR/../../pkg/esbuild-dev.bin.js --supervise --commands $DIR/run.ts
