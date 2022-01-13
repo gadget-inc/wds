8a3035ce5e6ccf4d9f92d111cdfe15a76cabfedd
Guillaume Malette <gmalette@gmail.com>
2022-01-13 15:59:12 -0500

    feat(swc): use 'swc' config from esbuild-dev.js

---
+++ spec/fixtures/src/esbuild-dev.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "swc": {
+    "jsc": {
+      "parser": {
+        "syntax": "typescript",
+        "decorators": true,
+        "dynamicImport": true
+      },
+      "target": "es2020"
+    },
+    "module": {
+      "type": "commonjs",
+      "strictMode": true,
+      "lazy": true
+    }
+  }
+}
---
+++ spec/fixtures/src/lazy_import.ts
@@ -0,0 +1,5 @@
+import { spawn } from "child_process";
+
+export function reSpawn() {
+  return spawn;
+}
