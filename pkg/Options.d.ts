2087f26cc5b4bb65b89a6e9fa73e055fb219f8c1
Harry Brundage <harry.brundage@gmail.com>
2022-01-24 17:34:45 -0500

    gitpkg

---
+++ pkg/Options.d.ts
@@ -0,0 +1,13 @@
+import { BuildOptions } from "esbuild";
+export interface RunOptions {
+    argv: string[];
+    terminalCommands: boolean;
+    reloadOnChanges: boolean;
+    supervise: boolean;
+    useSwc: boolean;
+}
+export interface ProjectConfig {
+    ignore: string[];
+    esbuild?: BuildOptions;
+    extensions: string[];
+}
---
+++ pkg/index.d.ts
@@ -0,0 +1,3 @@
+import { RunOptions } from "./Options";
+export declare const cli: () => Promise<void>;
+export declare const esbuildDev: (options: RunOptions) => Promise<void>;
