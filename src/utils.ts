fd9d78bdf49bcc633f7f89674aa615c47df97621
Harry Brundage <harry.brundage@gmail.com>
2024-03-21 22:23:51 -0400

    Use graceful-fs to recover from too many open files errors

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { promises as fs } from "fs";
+import { promises as fs } from "graceful-fs";
 import { defaults } from "lodash";
 import path from "path";
 import type { ProjectConfig } from "./Options";
